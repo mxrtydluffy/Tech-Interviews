a5e95e1b8623f85199106849556730f486637504
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-04-12 08:54:35 -0700

    [UPDATE] Original Question

--- original_question.py
+++ original_question.py
@@ -4,18 +4,19 @@
 
 #______________________________________________________________________________________________________________________________________
 
-# Overview
+#                                                       Overview
 
-# split function will give us a list of the four words.
+#  * split function will give us a list of the four words.
 
-# 2 hashmaps to go from a letter to a word and a word to a letter
-# a word needs to map to a single character. two words can't map to a single character
-# time complexity is O(n+m) based on the total amount of character have on each strings.
+# * 2 hashmaps to go from a letter to a word and a word to a letter
+# * A word needs to map to a single character. 
+# * Two words can't map to a single character
+# * Time complexity is O(n+m) based on the total amount of character have on each strings.
 
 #______________________________________________________________________________________________________________________________________
 
-def caffeine(pattern: str, s: str) -> bool:
-    # get list of words get built in function for the string s and split it via space character.
+def problem(pattern: str, s: str) -> bool:
+    # Get list of words for the string s and split it via space character.
     words = s.split(" ")
     # Then check if its going to be a valid solution.
     # Characters in pattern should be eqaul to the number of words we have
@@ -23,9 +24,9 @@ def caffeine(pattern: str, s: str) -> bool:
     if len(pattern) != len(words):
         return False
     
-    # Initilize hashmaps to Map each character to the word
+    # Initilize hashmaps to Map each character to the word.
     charToWord = {}
-    # Initilize hashmaps to Map each word to the character 
+    # Initilize hashmaps to Map each word to the character. 
     wordToChar = {}
 
     # Iterate through both of the characters in patten and each word in our list at the same exact time
@@ -34,13 +35,13 @@ def caffeine(pattern: str, s: str) -> bool:
     for c, w in zip(pattern, words):
         # Check if this character maps to a different word.
         # Therefore looking c has a character in hashmap and the word that it maps to is not equal 
-            # to this word we're currently looking at
+            # to this word we're currently looking at.
         
         if c in charToWord and charToWord[c] != w:
             # Returns False if patterns don't match.
             return False
         
-        # Do the same for the opposide data structure.
+        # Do the same for the opposite data structure.
         if w in wordToChar and wordToChar[w] != c:
             # Returns False if patterns don't match.
             return False
@@ -51,8 +52,6 @@ def caffeine(pattern: str, s: str) -> bool:
     # If there are no mismataches the patterns match so we can return true
     return True
 
-print(caffeine("yzyz","apples oranges apples oranges"))
-print(caffeine("abba","apples oranges apples oranges"))
-print(caffeine("popo","ramen boba ramen boba"))
-
-
+print(problem("yzyz","apples oranges apples oranges"))
+print(problem("abba","apples oranges apples oranges"))
+print(problem("popo","ramen boba ramen boba"))
